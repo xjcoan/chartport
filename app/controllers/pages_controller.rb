7f3a8186867459679ccf9d7af92ea255cdc04d7a
X Coan <xjcoan@gmail.com>
2016-11-04 22:46:09 -0500

    Add static pages not requiring users.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,11 @@
 class PagesController < ApplicationController
+
+  def index
+    @page_title = "Home"
+  end
+
+  def about
+    @page_title = "About"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # define root page
+  root 'pages#index'
+
+  get '/about' => 'pages#about'
 end
