c54f2bf50a34459ff899798a92e0d9053a10d9d2
X Coan <xjcoan@gmail.com>
2017-01-05 20:57:38 -0600

    Add tooltips to all. Change to pure css.

--- app/assets/javascripts/patients.js
+++ app/assets/javascripts/patients.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-  $('.namelink').each(function() {
-    $(this).hover(function() {
-      $(this).find('.popup').css("display", "inline");
-    }, function() {
-      $(this).find('.popup').css("display", "none");
-    });
-  });
-});
+
