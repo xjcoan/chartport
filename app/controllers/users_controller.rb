1c40b3d79540aab4d517e7dce0de9498c9af7fd9
X Coan <xjcoan@gmail.com>
2017-02-10 16:47:26 -0500

    Modify user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to dashboard_path
     else
       render 'new'
     end
