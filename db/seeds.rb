31c3a45b353f9de8e58c9c66439a0024b8c94121
X Coan <xjcoan@gmail.com>
2017-04-04 14:23:39 -0400

    Add db seed accounts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'SysAdmin', email: "admin@test.com", admin: true, password: 'test')
+User.create(name: 'Test Doc', email: "test@test.com", admin: false, password: 'test')
